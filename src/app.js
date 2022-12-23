6264081dbf7de48ec59712eb7cc191c581fe56c2
SaminPashaei <saminpashaei96@gmail.com>
2022-12-24 00:36:00 +0330

    Fixing wind direction function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
+function windDirection(degree) {
+  if ((degree >= 0 && degree <= 23) || (degree >= 337 && degree <= 360)) {
+    return "N";
+  } else if (degree >= 24 && degree <= 68) {
+    return "NE";
+  } else if (degree >= 69 && degree <= 113) {
+    return "E";
+  } else if (degree >= 114 && degree <= 158) {
+    return "SE";
+  } else if (degree >= 159 && degree <= 203) {
+    return "S";
+  } else if (degree >= 204 && degree <= 248) {
+    return "SW";
+  } else if (degree >= 249 && degree <= 293) {
+    return "W";
+  } else {
+    return "NW";
+  }
+}
+
 function twoDigit(number) {
   return String(number).padStart(2, "0");
 }
@@ -48,6 +68,9 @@ function getWeatherData(response) {
     response.data.wind.speed;
   document.querySelector("#current-wind-degree").innerHTML =
     response.data.wind.deg;
+  document.querySelector("#current-wind-direction").innerHTML = windDirection(
+    response.data.wind.deg
+  );
 }
 
 function callApi(input) {
