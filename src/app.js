c3fc4f6d4ce6d8228c8993b81d71cdf0ded89b16
SaminPashaei <saminpashaei96@gmail.com>
2022-12-30 12:01:32 +0330

    Using one call API

--- src/app.js
+++ src/app.js
@@ -1,28 +1,19 @@
-function mostSearched() {
-  let mostSearchedCity = document.querySelectorAll(".most-searched-city");
+function searchEngine() {
+  document
+    .querySelector("#search-city-form")
+    .addEventListener("submit", fixSearchedCity);
 
+  let mostSearchedCity = document.querySelectorAll(".most-searched-city");
   for (let index = 0; index < mostSearchedCity.length; index++) {
     mostSearchedCity[index].addEventListener("click", function (event) {
       event.preventDefault();
-      callApi(mostSearchedCity[index].textContent);
+      getCityApi(mostSearchedCity[index].textContent);
     });
   }
-}
 
-function getPosition(position) {
-  let currentPosition = [position.coords.latitude, position.coords.longitude];
-  callApi(currentPosition);
-}
-
-function geolocationError() {
-  alert("Unfortunately, we don't have access to your location.");
-}
-
-function findLocation() {
-  if (!navigator.geolocation) {
-    alert("Geolocation API is not supported by this browser.");
-  } else {
-    navigator.geolocation.getCurrentPosition(getPosition, geolocationError);
+  let locationButton = document.querySelectorAll(".location-button");
+  for (let button = 0; button < 2; button++) {
+    locationButton[button].addEventListener("click", findLocation);
   }
 }
 
@@ -35,21 +26,80 @@ function fixSearchedCity(event) {
   document.querySelector("#searched-city-name").value = "";
 
   if (searchedCityName.length > 0) {
-    callApi(searchedCityName);
+    getCityApi(searchedCityName);
   } else {
     alert("You didn't enter a city. Please try again.");
   }
 }
 
-function searchEngine() {
-  document
-    .querySelector("#search-city-form")
-    .addEventListener("submit", fixSearchedCity);
-
-  let locationButton = document.querySelectorAll(".location-button");
-  for (let button = 0; button < 2; button++) {
-    locationButton[button].addEventListener("click", findLocation);
+function findLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      getCityApi([position.coords.latitude, position.coords.longitude]);
+    }, geolocationError);
+  } else {
+    alert("Geolocation API is not supported by this browser.");
   }
+}
+
+function geolocationError() {
+  alert("Unfortunately, we don't have access to your location.");
+}
+
+function getCityApi(input) {
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let usedUnit = "metric";
+  let searchedInput;
+
+  if (Array.isArray(input)) {
+    searchedInput = `lat=${input[0]}&lon=${input[1]}`;
+  } else {
+    searchedInput = `q=${input}`;
+  }
+
+  let apiUrl = `${apiEndpoint}?${searchedInput}&appid=${apiKey}&units=${usedUnit}`;
+  axios.get(apiUrl).then(changeCityName, apiError);
+}
+
+function apiError() {
+  alert("Unfortunately, we don't have weather information for this city.");
+}
+
+function changeCityName(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  oneCallApi(response.data.coord.lat, response.data.coord.lon);
+}
+
+function oneCallApi(lat, lon) {
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/onecall";
+  let exclude = "minutely,hourly,alerts";
+  let usedUnit = "metric";
+  let apiUrl = `${apiEndpoint}?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${apiKey}&units=${usedUnit}`;
+  axios.get(apiUrl).then(getWeatherData);
+}
+
+function getWeatherData(response) {
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#current-humidity").innerHTML =
+    response.data.main.humidity;
+  document.querySelector("#current-pressure").innerHTML =
+    response.data.main.pressure;
+  document.querySelector("#current-wind-speed").innerHTML =
+    response.data.wind.speed;
+  document.querySelector("#current-wind-degree").innerHTML =
+    response.data.wind.deg;
+  document.querySelector("#current-wind-direction").innerHTML = windDirection(
+    response.data.wind.deg
+  );
+
+  chnageIcon(response.data.weather[0].icon, response.data.weather[0].main);
+  changeDate(new Date(response.data.dt * 1000));
+  changeHorizon(
+    response.data.sys.sunrise * 1000,
+    response.data.sys.sunset * 1000
+  );
+  changeTemperature(response.data.main);
 }
 
 function showFahrenheit() {
@@ -172,51 +222,6 @@ function windDirection(degree) {
   }
 }
 
-function getWeatherData(response) {
-  document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector("#weather-description").innerHTML =
-    response.data.weather[0].description;
-  document.querySelector("#current-humidity").innerHTML =
-    response.data.main.humidity;
-  document.querySelector("#current-pressure").innerHTML =
-    response.data.main.pressure;
-  document.querySelector("#current-wind-speed").innerHTML =
-    response.data.wind.speed;
-  document.querySelector("#current-wind-degree").innerHTML =
-    response.data.wind.deg;
-  document.querySelector("#current-wind-direction").innerHTML = windDirection(
-    response.data.wind.deg
-  );
-
-  chnageIcon(response.data.weather[0].icon, response.data.weather[0].main);
-  changeDate(new Date(response.data.dt * 1000));
-  changeHorizon(
-    response.data.sys.sunrise * 1000,
-    response.data.sys.sunset * 1000
-  );
-  changeTemperature(response.data.main);
-}
-
-function apiError() {
-  alert("Unfortunately, we don't have weather information for this city.");
-}
-
-function callApi(input) {
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiKey = "9c76791214fe9c90d314ec57f7743bcd";
-  let usedUnit = "metric";
-  let searchedInput;
-
-  if (Array.isArray(input)) {
-    searchedInput = `lat=${input[0]}&lon=${input[1]}`;
-  } else {
-    searchedInput = `q=${input}`;
-  }
-
-  let apiUrl = `${apiEndpoint}?${searchedInput}&appid=${apiKey}&units=${usedUnit}`;
-  axios.get(apiUrl).then(getWeatherData, apiError);
-}
-
 let apiKey = "7746bdeabca928cfedcad71e52fd9d66";
 let centigradeDegree,
   fahrenheitDegree,
@@ -227,7 +232,6 @@ let centigradeDegree,
   centigradeTemp,
   fahrenheitTemp;
 
-callApi("Frankfurt");
+getCityApi("Frankfurt");
 changeScale();
 searchEngine();
-mostSearched();
